83cd4c878fef16c72641b8017859956f6e39db70
Simon Chung <simon.chung@rdkcentral.com>
2025-02-26 17:27:09 +0000

    Create Coverity issue

--- NetworkManagerRDKProxy.cpp
+++ NetworkManagerRDKProxy.cpp
@@ -1167,7 +1167,7 @@ const string CIDR_PREFIXES[CIDR_NETMASK_IP_LEN+1] = {
                 NMLOG_DEBUG("Scan SSIDs of frequency %s", m_filterfrequency.c_str());
             }
 
-            memset(&param, 0, sizeof(param));
+            memset(&param, 0, 10*sizeof(param));
 
             retVal = IARM_Bus_Call(IARM_BUS_NM_SRV_MGR_NAME, IARM_BUS_WIFI_MGR_API_getAvailableSSIDsAsync, (void *)&param, sizeof(IARM_Bus_WiFiSrvMgr_SsidList_Param_t));
 
