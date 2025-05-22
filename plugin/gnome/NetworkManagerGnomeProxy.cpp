b1ba3cf9c0186104e86e4603425ed8f629344358
RAFI <103924677+cmuhammedrafi@users.noreply.github.com>
2025-05-23 01:18:24 +0530

    RDKEMW-4175 : WiFi Power Switch toggling On/Off support (#154)
    Reason for change:  for following issues
    	- RDKEMW-3859 SetInterfaceState is not working as expected for the wlan0 interface.
    	- RDKEMW-4063 [RDKE][Gnome]WiFi is connected on bootup even if WiFi option was disabled before reboot
    	- RDKEMW-4106 [RDKEMW][FOXTEL]Device does not automatically reconnect to WiFi SSID after toggling WiFi OFF and ON
    	- RDKEMW-4175 WiFi doesn't connect back automatically to previously connected SSID after toggling WiFi On/Off Switch
    
    Test Procedure: Verify wifi disable option in RDKE device

--- plugin/gnome/NetworkManagerGnomeProxy.cpp
+++ plugin/gnome/NetworkManagerGnomeProxy.cpp
@@ -96,7 +96,6 @@ namespace WPEFramework
             nmEvent = GnomeNetworkManagerEvents::getInstance();
             nmEvent->startNetworkMangerEventMonitor();
             wifi = wifiManager::getInstance();
-            return;
         }
 
         uint32_t NetworkManagerImplementation::GetAvailableInterfaces (Exchange::INetworkManager::IInterfaceDetailsIterator*& interfacesItr/* @out */)
@@ -254,6 +253,22 @@ namespace WPEFramework
             }
 
             NMLOG_INFO("interface %s state: %s", interface.c_str(), enabled ? "enabled" : "disabled");
+            if(enabled)
+            {
+                sleep(1); // wait for 1 sec to change the device state
+                if(interface == nmUtils::wlanIface() && _instance != NULL)
+                {
+                    NMLOG_INFO("Activating connection '%s' ...", _instance->m_lastConnectedSSID.c_str());
+                    wifi->activateKnownConnection(nmUtils::wlanIface(), _instance->m_lastConnectedSSID);
+                }
+                else if(interface == nmUtils::ethIface())
+                {
+                    NMLOG_INFO("Activating connection 'Wired connection 1' ...");
+                    // default wired connection name is 'Wired connection 1'
+                    wifi->activateKnownConnection(nmUtils::ethIface(), "Wired connection 1");
+                }
+            }
+
             return Core::ERROR_NONE;
         }
 
@@ -644,7 +659,7 @@ namespace WPEFramework
             if(ssid.ssid.empty() && _instance != NULL)
             {
                 NMLOG_WARNING("ssid is empty activating last connectd ssid !");
-                if(wifi->activateKnownWifiConnection(_instance->m_lastConnectedSSID))
+                if(wifi->activateKnownConnection(nmUtils::wlanIface(), _instance->m_lastConnectedSSID))
                     rc = Core::ERROR_NONE;
             }
             else if(ssid.ssid.size() < 32)
