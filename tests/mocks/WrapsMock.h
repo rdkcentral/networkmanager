4948b8ad261734c8d2ef26d5248d8e1131627347
RAFI <103924677+cmuhammedrafi@users.noreply.github.com>
2025-07-27 00:30:16 +0530

    RDKEMW-5756 : NetworkManager Plugin - L1/L2 Test coverage -  RDK Backend (#199)
    * RDKEMW-5756 NetworkManager Plugin - Improve L1/L2 Test coverage - NetSrvMgr Backend
    
    * more Test case added
    * popen mock add
    * getWifiSiganalStrength gtest added
    * setipsettings
    * test
    * update the wraps mock

---
+++ tests/mocks/WrapsMock.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <gmock/gmock.h>
+#include <mntent.h>
+
+#include "Wraps.h"
+
+extern "C" FILE* __real_setmntent(const char* command, const char* type);
+
+class WrapsImplMock : public WrapsImpl {
+public:
+    WrapsImplMock():WrapsImpl()
+    {
+	   /*Setting up Default behavior for setmntent: 
+        * We are mocking setmntent in this file below with __wrap_setmntent,
+        * and the actual setmntent will be called via this interface */
+        ON_CALL(*this, setmntent(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke(
+            [&](const char* command, const char* type) -> FILE* {
+                return __real_setmntent(command, type);
+            }));
+    }
+    virtual ~WrapsImplMock() = default;
+
+    MOCK_METHOD(int, system, (const char* command), (override));
+    MOCK_METHOD(FILE*, popen, (const char* command, const char* type), (override));
+    MOCK_METHOD(int, pclose, (FILE* pipe), (override));
+    MOCK_METHOD(void, syslog, (int pri, const char* fmt, va_list args), (override));
+    MOCK_METHOD(FILE*, setmntent, (const char* command, const char* type), (override));
+    MOCK_METHOD(struct mntent*, getmntent, (FILE* pipe), (override));
+    MOCK_METHOD(FILE*, v_secure_popen, (const char *direction, const char *command, va_list args), (override));
+    MOCK_METHOD(int, v_secure_pclose, (FILE *file), (override));
+    MOCK_METHOD(int, v_secure_system,(const char *command, va_list args), (override));
+    MOCK_METHOD(ssize_t, readlink, (const char *pathname, char *buf, size_t bufsiz), (override));
+    MOCK_METHOD(time_t, time, (time_t* arg), (override));
+};
---
+++ tests/mocks/thunder/CommunicatorMock.h
@@ -0,0 +1,53 @@
+/*
+* If not stated otherwise in this file or this component's LICENSE file the
+* following copyright and licenses apply:
+*
+* Copyright 2025 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#pragma once
+
+#include <gmock/gmock.h>
+#include "Communicator.h"
+
+#if 0
+class CommunicatorClientMock : public ICommunicatorClient
+{
+public:
+    virtual ~CommunicatorClientMock() = default;
+
+    MOCK_METHOD(uint32_t, Open, (const uint32_t waitTime), (override));
+    MOCK_METHOD(INTERFACE*, Open, (const std::string& className, const uint32_t version, const uint32_t waitTime), (override));
+    MOCK_METHOD(INTERFACE*, Acquire, (const uint32_t waitTime, const std::string& className, const uint32_t versionId), (override));
+    MOCK_METHOD(uint32_t, Offer, (INTERFACE* offer, const uint32_t version, const uint32_t waitTime), (override));
+    MOCK_METHOD(uint32_t, Revoke, (INTERFACE* offer, const uint32_t version, const uint32_t waitTime), (override));
+    MOCK_METHOD(uint32_t, Close, (const uint32_t waitTime), (override));
+};
+#else
+class CommunicatorClientMock : public ICommunicatorClient {
+public:
+    virtual ~CommunicatorClientMock() = default;
+    MOCK_METHOD(void, AddRef, (), (const));
+    MOCK_METHOD(uint32_t, Release, (), (const));
+    MOCK_METHOD(uint32_t, Open, (const uint32_t waitTime), (override));
+    MOCK_METHOD(void*, Open, (const std::string& className, const uint32_t version, const uint32_t waitTime), (override));
+    MOCK_METHOD(void*, Acquire, (const uint32_t waitTime, const std::string& className, const uint32_t versionId), (override));
+    MOCK_METHOD(uint32_t, Offer, (void* offer, const uint32_t version, const uint32_t waitTime), (override));
+    MOCK_METHOD(uint32_t, Revoke, (void* offer, const uint32_t version, const uint32_t waitTime), (override));
+    MOCK_METHOD(uint32_t, Close, (const uint32_t waitTime), (override));
+    MOCK_METHOD(void*, Acquire, (const std::string& className, const uint32_t interfaceId, const uint32_t versionId), (override));
+};
+
+#endif
