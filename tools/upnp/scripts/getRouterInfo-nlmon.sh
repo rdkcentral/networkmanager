4f23e7c0593adfdff5c4a85920e38333624a5de5
jincysam87 <167995204+jincysam87@users.noreply.github.com>
2025-08-19 22:48:04 -0400

    RDK-56442: Integrate UPNP to NetworkManagerPlugin (#196)
    * RDK-56442: Integrate UPNP to NetworkManagerPlugin
    
    Reason for change: Using gupnp library to fetch gateway details
    Test Procedure: Gateway details will be available in log file
    Risks: Low
    Signed-off-by: jincysaramma_sam@comcast.com
    
    * Update getRouterInfo-nlmon.sh
    
    * Update readyToGetRouterInfo.sh
    
    * Update getRouterInfo-nlmon.sh
    
    * Update getRouterInfo-nlmon.sh
    
    * Update getRouterInfo-NMdispatcher.sh
    
    * Update getRouterInfo-nlmon.sh
    
    * Update getRouterInfo-nlmon.sh
    
    * Update getRouterInfo-NMdispatcher.sh
    
    * Update getRouterInfo-nlmon.sh
    
    * Update getRouterInfo-NMdispatcher.sh
    
    ---------
    
    Signed-off-by: jincysaramma_sam@comcast.com

---
+++ tools/upnp/scripts/getRouterInfo-nlmon.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+##############################################################################
+# If not stated otherwise in this file or this component's LICENSE file the
+# following copyright and licenses apply:
+#
+# Copyright 2025 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+. /etc/device.properties
+
+cmd=$1
+mode=$2
+ifc=$3
+flags=$5
+file="/tmp/.routerdiscover"
+if [[ "$ifc" == "$WIFI_INTERFACE" || "$ifc" == "$ETHERNET_INTERFACE" ]]; then
+    if [ "$cmd" == "add" ] && [ "$flags" = "global" ] && [ "$mode" = "ipv4" ]; then
+        if [ ! -f $file ]; then
+            touch /tmp/.routerdiscover
+            # Find the PID of the routerDiscovery process that is running specifically on this interface.
+            pid=`pidof routerDiscovery`
+            if [ -n "$pid" ]; then
+                echo "$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ") Found existing routerDiscovery ($pid) for $ifc, Killing it." >> /opt/logs/routerInfo.log
+                kill -15 "$pid"
+            fi
+            echo "$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ") Starting routerDiscovery on $ifc" >> /opt/logs/routerInfo.log
+            systemctl start routerDiscovery@$ifc
+        fi
+    fi
+fi
