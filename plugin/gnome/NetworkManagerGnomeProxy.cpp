d79673bc462c031a57020006806b7d87f85f171b
gururaajar <83449026+gururaajar@users.noreply.github.com>
2025-08-27 16:41:05 -0400

    RDKEMW-5842 - NetworkManager Plugin - Coverity Improvements (#213)
    * RDKEMW-5842 - NetworkManager Plugin - Coverity Improvements
    
    Reason for change: Addressed the networkmanager coverity issue
    reported in the last received full scan report
    Test Procedure: Coverity check
    Risks: Medium
    Signed-off-by: Gururaaja ESR <Gururaja_ErodeSriranganRamlingham@comcast.com>
    Co-authored-by: Karunakaran A <48997923+karuna2git@users.noreply.github.com>

--- plugin/gnome/NetworkManagerGnomeProxy.cpp
+++ plugin/gnome/NetworkManagerGnomeProxy.cpp
@@ -315,8 +315,11 @@ namespace WPEFramework
                     if(ifaceStr == wifiname || ifaceStr == ethname) // only wifi and ethenet taking
                     {
                         NMDeviceState deviceState = NM_DEVICE_STATE_UNKNOWN;
-                        Exchange::INetworkManager::InterfaceDetails interface;
-                        interface.mac = nm_device_get_hw_address(device);
+                        Exchange::INetworkManager::InterfaceDetails interface{};
+                        const char* macAddr = nm_device_get_hw_address(device);
+                        if(macAddr != nullptr) {
+                            interface.mac = macAddr;
+                        }
                         deviceState = nm_device_get_state(device);
                         interface.enabled = (deviceState >= NM_DEVICE_STATE_UNAVAILABLE)? true : false;
                         if(deviceState > NM_DEVICE_STATE_DISCONNECTED && deviceState < NM_DEVICE_STATE_DEACTIVATING)
@@ -392,21 +395,23 @@ namespace WPEFramework
                 NMLOG_ERROR("nm_connection_get_interface_name is failed");
                 /* Temporary mitigation for nm_connection_get_interface_name failure */
                 if(m_ethConnected.load())
-                    ifacePtr = ethname.c_str();
+                    interface = ethname;
                 else // default always wifi
-                    ifacePtr = wifiname.c_str();
-            }
-
-            interface = ifacePtr;
-            m_defaultInterface = interface;
-            if(interface != wifiname && interface != ethname)
-            {
-                NMLOG_ERROR("primary interface is not eth/wlan");
-                interface.clear();
+                    interface = wifiname;
+                rc = Core::ERROR_NONE;
             }
             else
-                rc = Core::ERROR_NONE;
-
+            {
+                interface = ifacePtr;
+                if(interface != wifiname && interface != ethname)
+                {
+                    NMLOG_ERROR("primary interface is not Ethernet or WiFi");
+                    interface.clear();
+                }
+                else
+                    rc = Core::ERROR_NONE;
+            } 
+            m_defaultInterface = interface;
             return rc;
         }
 
