3bfcea018e54ebf9c15097d8928ec4f040f06428
Muhammed Rafi c <muhammed_rafi@comcast.com>
2025-10-17 16:44:13 +0530

    remove un wanted changed

--- plugin/gnome/NetworkManagerGnomeProxy.cpp
+++ plugin/gnome/NetworkManagerGnomeProxy.cpp
@@ -366,29 +366,43 @@ namespace WPEFramework
                 return Core::ERROR_GENERAL;
             }
 
-            remoteConn = nm_active_connection_get_connection(activeConn);
-            if(remoteConn != NULL)
+            if(!m_wlanEnabled.load() && !m_ethEnabled.load())
             {
-                const char *ifacePtr = nm_connection_get_interface_name(NM_CONNECTION(remoteConn));
-                if(ifacePtr != NULL)
+                NMLOG_INFO("Both iface disabled state, returning no primary interface");
+                interface.clear();
+                m_defaultInterface = interface;
+                return Core::ERROR_NONE;
+            }
+
+            activeConn = nm_client_get_primary_connection(client);
+            if (activeConn != NULL)
+            {
+                remoteConn = nm_active_connection_get_connection(activeConn);
+                if(remoteConn != NULL)
                 {
-                    interface = ifacePtr;
-                    if(interface != nmUtils::wlanIface() && interface != nmUtils::ethIface())
+                    const char *ifacePtr = nm_connection_get_interface_name(NM_CONNECTION(remoteConn));
+                    if(ifacePtr != NULL)
                     {
-                        NMLOG_WARNING("primary interface is not Ethernet or WiFi");
-                        interface.clear();
+                        interface = ifacePtr;
+                        if(interface != nmUtils::wlanIface() && interface != nmUtils::ethIface())
+                        {
+                            NMLOG_WARNING("primary interface is not Ethernet or WiFi");
+                            interface.clear();
+                        }
+                        else
+                        {
+                            NMLOG_DEBUG("primary interface is %s", interface.c_str());
+                            rc = Core::ERROR_NONE;
+                        }
                     }
                     else
-                    {
-                        NMLOG_DEBUG("primary interface is %s", interface.c_str());
-                        rc = Core::ERROR_NONE;
-                    }
+                        NMLOG_WARNING("interface name is missing form connection");
                 }
                 else
-                    NMLOG_WARNING("interface name is missing form connection");
+                    NMLOG_WARNING("primary connection but remote connection error");
             }
             else
-                NMLOG_WARNING("primary connection but remote connection error");
+                NMLOG_WARNING("No primary connection");
 
             if(rc != Core::ERROR_NONE)
             {
