9a498c039e9a5137a954aed693c51d85919faecc
jincysam87 <167995204+jincysam87@users.noreply.github.com>
2026-06-24 10:47:24 -0400

    Update NetworkManagerImplementation.h

--- plugin/NetworkManagerImplementation.h
+++ plugin/NetworkManagerImplementation.h
@@ -45,7 +45,7 @@ using namespace std;
 #define DEFAULT_NOISE                              -96
 #define MAX_SNR_VALUE                              180
 
-#define DEFAULT_WIFI_SIGNAL_TEST_INTERVAL_SEC      60
+#define DEFAULT_WIFI_SIGNAL_TEST_INTERVAL_SEC      5
 #define NM_PROCESS_MONITOR_INTERVAL_SEC            60
 #define NM_WIFI_SNR_THRESHOLD_EXCELLENT            40
 #define NM_WIFI_SNR_THRESHOLD_GOOD                 25
