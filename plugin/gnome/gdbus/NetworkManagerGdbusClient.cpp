1b14637d1af3b9186195053d3aba3fc30111b83f
mpalan315 <Mehavarshni_Palaniswamy@comcast.com>
2026-08-27 10:13:45 +0530

    RDKEMW-22842: onWiFiStateChange Event to Include Wi-Fi Profile Information
    Reason for change: onWiFiStateChange event currently reports only the Wi-Fi connection state.
    Priority: P1
    Test Procedure: Refer ticket
    Risks: Low
    
    Signed-off-by: Mehavarshni_Palaniswamy@comcast.com

--- plugin/gnome/gdbus/NetworkManagerGdbusClient.cpp
+++ plugin/gnome/gdbus/NetworkManagerGdbusClient.cpp
@@ -2501,7 +2501,7 @@ namespace WPEFramework
                 {
                     NMLOG_INFO("'%s' already connected !", currentSSID.ssid.c_str());
                     if(_instance != nullptr)
-                        _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTED);
+                        _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTED, currentSSID.ssid);
                     return true;
                 }
                 else
@@ -2987,7 +2987,7 @@ namespace WPEFramework
                     {
                         NMLOG_WARNING("WPS process failed - device in disconnected state");
                         if(_instance != nullptr)
-                            _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_SSID_NOT_FOUND);
+                            _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_SSID_NOT_FOUND, ssidinfo.ssid);
                         break;
                     }
                     else if(devProperty.state > NM_DEVICE_STATE_NEED_AUTH)
@@ -2995,7 +2995,7 @@ namespace WPEFramework
                         NMLOG_INFO("WPS process completed successfully");
                         wpsComplete = true;
                         if(_instance != nullptr)
-                            _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTED);
+                            _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTED, ssidinfo.ssid);
                         break;
                     }
 
@@ -3004,7 +3004,7 @@ namespace WPEFramework
                     {
                         NMLOG_ERROR("WPS process failed - timeout");
                         if(_instance != nullptr)
-                            _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTION_FAILED);
+                            _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTION_FAILED, ssidinfo.ssid);
                         break;
                     }
                     continue;
@@ -3028,7 +3028,7 @@ namespace WPEFramework
                     NMLOG_INFO("WPS process stopped - already connected to WPS AP '%s'", ssidinfo.ssid.c_str());
                     wpsComplete = true;
                     if(_instance != nullptr)
-                        _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTED);
+                        _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTED, ssidinfo.ssid);
                     break;
                 }
 
@@ -3064,13 +3064,13 @@ namespace WPEFramework
             {
                 NMLOG_WARNING("WPS AP not found");
                 if(_instance != nullptr)
-                    _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_SSID_NOT_FOUND);
+                    _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_SSID_NOT_FOUND, ssidinfo.ssid);
             }
             else if(!wpsComplete && m_wpsActionTriggered)
             {
                 NMLOG_INFO("WPS process error");
                 if(_instance != nullptr)
-                    _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTION_FAILED);
+                    _instance->ReportWiFiStateChange(Exchange::INetworkManager::WIFI_STATE_CONNECTION_FAILED, ssidinfo.ssid);
             }
 
             NMLOG_INFO("WPS process thread exit");
